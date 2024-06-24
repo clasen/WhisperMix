392dadd8d7c64100a5ac6e9e968e9237398356cb
clasen <martin.clasen@gmail.com>
2024-06-24 13:06:51 -0300

    Update groq.js

--- demo/groq.js
+++ demo/groq.js
@@ -3,6 +3,6 @@ const WhisperMix = require('../index.js')
 const transcribe = new WhisperMix({ model: 'whisper-large-v3' });
 
 main(); async function main() {
-    const r = transcribe.fromVoiceFile('./example.mp3');
+    const r = await transcribe.fromVoiceFile('./example.mp3');
     console.log(r)
 }
